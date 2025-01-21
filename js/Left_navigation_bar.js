3e69d48996d276e95069cdde13952ebf6e45b8f9
yi <978950325@qq.com>
2025-01-21 21:15:38 +0800

    20250121_2116：1. list 页面 太乱了 移动端下处理好大小和位置（已完成） 2.
    其他修改：
    
    1. 搜索页面字体移动端放大与其他页面统一（已完成）
    2. 内容页面左侧JS失效问题解决（已完成）
    3. search整体的页面美化（已完成）

--- js/Left_navigation_bar.js
+++ js/Left_navigation_bar.js
@@ -1,13 +1,22 @@
+// 当文档内容完全加载和解析完成时执行以下函数
 document.addEventListener("DOMContentLoaded", function () {
+    // 获取所有具有类名 "Left_navigation_bar_item" 的元素，并存储在 menuItems 变量中
     const menuItems = document.querySelectorAll(".Left_navigation_bar_item");
+    // 遍历每一个菜单项
     menuItems.forEach(item => {
+        // 获取当前菜单项中的具有类名 "Left_navigation_bar_title" 的元素，并存储在 title 变量中
         const title = item.querySelector(".Left_navigation_bar_title");
+        // 为 title 元素添加点击事件监听器
         title.addEventListener("click", function () {
+            // 再次遍历所有菜单项
             menuItems.forEach(otherItem => {
+                // 如果当前遍历的菜单项不是被点击的菜单项
                 if (otherItem !== item) {
+                    // 移除该菜单项的 "active" 类
                     otherItem.classList.remove("active");
                 }
             });
+            // 切换被点击菜单项的 "active" 类（如果已有则移除，如果没有则添加）
             item.classList.toggle("active");
         });
     });
